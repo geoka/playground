a6ffb5b6455be147939b98b1248d7d4b4b2d7e89
geoka <geokallos@gmail.com>
2024-08-19 17:33:18 +0100

    BQ connection fix

--- ETL-Renewable-Energy-Consumption/scripts/db_engine_bigquery.py
+++ ETL-Renewable-Energy-Consumption/scripts/db_engine_bigquery.py
@@ -9,13 +9,13 @@ from langchain_community.utilities import SQLDatabase
 BIGQUERY_PROJECT = "ni-sadc-live"
 BIGQUERY_DATASET = "ni_gk_eu"
 SERVICE_ACCOUNT_KEY_PATH = 'C:/Users/Georgios Kallos/AppData/Roaming/gcloud/application_default_credentials.json'
-# SQL_ALCHEMY_URL = f'bigquery://{BIGQUERY_PROJECT}/{BIGQUERY_DATASET}?credentials_path={SERVICE_ACCOUNT_KEY_PATH}'
-# db = SQLDatabase.from_uri(SQL_ALCHEMY_URL)
+SQL_ALCHEMY_URL = f'bigquery://{BIGQUERY_PROJECT}/{BIGQUERY_DATASET}' #?credentials_path={SERVICE_ACCOUNT_KEY_PATH}'
+db = SQLDatabase.from_uri(SQL_ALCHEMY_URL)
 
-engine = create_engine('bigquery://', credentials_path='C:/Users/Georgios Kallos/AppData/Roaming/gcloud/application_default_credentials.json')
+# engine = create_engine('bigquery://', credentials_path='C:/Users/Georgios Kallos/AppData/Roaming/gcloud/application_default_credentials.json')
 
 # Create a language model
-# llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
 # Create the SQL agent
-# agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
+agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
