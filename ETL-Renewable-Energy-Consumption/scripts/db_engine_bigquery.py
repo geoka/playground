fea6b8fd04fcf06c1873f3c231b3acb7c91a0c20
geoka <geokallos@gmail.com>
2024-08-20 15:02:32 +0100

    vertex AI integration

--- ETL-Renewable-Energy-Consumption/scripts/db_engine_bigquery.py
+++ ETL-Renewable-Energy-Consumption/scripts/db_engine_bigquery.py
@@ -1,6 +1,6 @@
-from sqlalchemy import *
+import os
 from langchain_openai import ChatOpenAI
-from langchain_community.agent_toolkits import SQLDatabaseToolkit,create_sql_agent
+from langchain_community.agent_toolkits import create_sql_agent
 from langchain_community.utilities import SQLDatabase
 
 
@@ -8,11 +8,10 @@ from langchain_community.utilities import SQLDatabase
 # db = SQLDatabase.from_uri("bigquery://")
 BIGQUERY_PROJECT = "ni-sadc-live"
 BIGQUERY_DATASET = "ni_gk_eu"
-SERVICE_ACCOUNT_KEY_PATH = 'C:/Users/Georgios Kallos/AppData/Roaming/gcloud/application_default_credentials.json'
-SQL_ALCHEMY_URL = f'bigquery://{BIGQUERY_PROJECT}/{BIGQUERY_DATASET}' #?credentials_path={SERVICE_ACCOUNT_KEY_PATH}'
-db = SQLDatabase.from_uri(SQL_ALCHEMY_URL)
 
-# engine = create_engine('bigquery://', credentials_path='C:/Users/Georgios Kallos/AppData/Roaming/gcloud/application_default_credentials.json')
+SERVICE_ACCOUNT_KEY_PATH = os.environ["SERVICE_ACCOUNT_KEY_PATH"]
+SQL = f'bigquery://{BIGQUERY_PROJECT}/{BIGQUERY_DATASET}' #?credentials_path={SERVICE_ACCOUNT_KEY_PATH}'
+db = SQLDatabase.from_uri(SQL)
 
 # Create a language model
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
